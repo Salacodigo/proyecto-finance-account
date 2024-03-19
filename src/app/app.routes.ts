a1e4f40cb54deded7b042238c05aa453731b2b99
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2024-03-19 15:49:47 -0500

    Initial Structure
    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './domains/shared/components/layout/layout.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./domains/home/home.component')
+            },
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./domains/transactions/components/dashboard/dashboard.component')
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./domains/profile/profile.component')
+            },
+            {
+                path: 'transaction-list',
+                loadComponent: () => import('./domains/transactions/components/transaction-list/transaction-list.component')
+            },
+            {
+                path: 'login',
+                loadComponent: () => import('./domains/authentication/components/login/login.component')
+            },
+            {
+                path: 'signup',
+                loadComponent: () => import('./domains/authentication/components/sign-up/sign-up.component')
+            },
+            {
+                path: 'accounts',
+                loadComponent: () => import('./domains/accounts/accounts.component')
+            },
+        ]
+    },
+    {
+        path: '**',
+        component: LayoutComponent
+    }
+    
+];
